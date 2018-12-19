83e4ae7230c23938ae02478408face0247595a9d
Arafat4341 <mdyasinarafatyen2@gmail.com>
2018-12-19 11:47:41 -0800

    Buffering the dataset

--- chatbot_database.py
+++ chatbot_database.py
@@ -12,6 +12,36 @@ def create_table():
 	c.execute(""" CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, 
 				comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT) """)
 
+def format_data(data):
+		data = data.replace("\n", " newlinechar ").replace("\r", " newlinechar ").replace('"', "'")
+		return data
+
+def find_parent(pid):
+	try:
+		sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1"
+		c.execute(sql)
+		result = c.fetchone()
+		if result != None:
+			return result[0]
+		else: return False
+	except Exception as e:
+		return False
+
 if __name__ == '__main__':
 	create_table()
-	print("Hey there!")
+	row_counter = 0
+	paired_rows = 0
+
+	with open('D:\git\Deep_learning_ChatBot_with_tensorflow\RC_2007-03', buffer=1000) as f:
+		for row in f:
+			row_counter += 1
+			row = json.loads(row)
+			parent_id = row['parent_id']
+			body = format_data(row['body'])
+			created_utc = row['created_utc']
+			score = row['score']
+			subreddit = row['subreddit']
+
+			parent_data = find_parent(parent_id)
+
+
